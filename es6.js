15a5b2f8aec310266ae8270ea02fa85f95ca7f35
Michael W Chung <michaelwchungstudio@gmail.com>
2019-06-29 16:04:39 -0400

    arrowfunc

---
+++ es6.js
@@ -0,0 +1,12 @@
+// ES6 JavaScript
+
+// let & const
+let - variable values, similar to var
+const - constant values, will not receive new values
+
+// Arrow Functions
+const myFnc = () => {
+  ...
+}
+
+const multiply = number => number * 2;
